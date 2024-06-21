bb1014fc1ab56a9c9b09ab3d4bdba70ce20b4c77
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-06-21 11:35:37 +0900

    Update script.js

--- assets/side_bar/script.js
+++ assets/side_bar/script.js
@@ -41,11 +41,3 @@ function toggleSidebar() {
         behavior: 'smooth'
       });
     }
-
-//Mキーが押されたら開閉する
-document.addEventListener('keydown', function(event) {
-    // "M"キーが押されたかを確認
-    if (event.key === 'M' || event.key === 'm') {
-        toggleSidebar();
-    }
-});

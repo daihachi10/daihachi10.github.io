0dd8e075e3fe90cac944ac98b5d13fd221833119
daihachi10sub <daihachi10sub@gmail.com>
2024-08-14 20:26:02 +0900

    Update script.js

--- search/assets/script.js
+++ search/assets/script.js
@@ -3,3 +3,10 @@ function searchGoogle() {
     var url = "https://www.google.com/search?q=" + encodeURIComponent(query);
     window.location.href = url;
 }
+
+// エンターキーで検索を実行する
+document.getElementById("search-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchGoogle();
+    }
+});

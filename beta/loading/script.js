92190e1791309a479a93c298d1a3187157c79be6
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-10-15 16:36:44 +0900

    Delete beta/loading directory

--- beta/loading/script.js
+++
@@ -1,7 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-document.title = "戻ってきて！";
-})
-window.addEventListener("focus", () => {
-document.title = "daihachi web";
-})

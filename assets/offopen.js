139b3e8e48dd2f0d2be20acfb8d312018ed7d5cc
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-10-18 20:37:47 +0900

    古いの全部消した
    古いの全部消した
    古いのはNASに入ってる

--- assets/offopen.js
+++
@@ -1,8 +0,0 @@
-        if (window.matchMedia('(display-mode: standalone)').matches) {
-  console.log("pwa");
-            if (!navigator.onLine) {
-                showNewNotification();
-            }
-    } else {
-      console.log("not pwa");
-      }
--- assets/side_bar/mkeyopen.js
+++
@@ -1,7 +0,0 @@
-//Mキーが押されたら開閉する
-document.addEventListener('keydown', function(event) {
-    // "M"キーが押されたかを確認
-    if (event.key === 'M' || event.key === 'm') {
-        toggleSidebar();
-    }
-});

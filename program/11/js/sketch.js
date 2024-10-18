ecbed9a2ca524cf4f19142e81dccdab7e0e60b3d
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-10-18 11:27:09 +0900

    Update sketch.js

--- program/11/js/sketch.js
+++ program/11/js/sketch.js
@@ -3,60 +3,47 @@ let x;
 let y;
 let ellipseSize;
 let fps = 10; //defo10
-let kaku = 0.001; //　デフォルトは0.001 ここいじる
+let speed = 0.001; //　デフォルトは0.001 ここいじる
 let size = 180; //　円の半径デフォルトは200
+
 function setup() {
-    let canvasContainer = document.getElementById("p5-canvas-container");
-    let fpsSlider = document.getElementById("fpsSlider");
-    // let kakuSlider = document.getElementById("speedSlider");
-    let canvas = createCanvas(400, 400);
-    canvas.parent(canvasContainer); // コンテナにキャンバスを配置
-    background("#fff");
-    // slider = createSlider(1, 255, fps, 5);
-    // slider.position(10, 10);
-    // slider.size(200);
-    noLoop();
+  let canvasContainer = document.getElementById("p5-canvas-container");
+  let canvas = createCanvas(400, 400);
+  canvas.parent(canvasContainer); // コンテナにキャンバスを配置
+  background("#fff");
+  noLoop();
 }
 
 function programStart() {
-    loop();
-    reset();
+  loop();
+  reset();
 }
 
 function draw() {
-    // lines("#000");
-    // let fps = slider.value();
-    let fps = fpsSlider.value
-    // let kaku = kakuSlider.value
-    frameRate(fps);
-    timer += kaku;
-    x = cos(timer * 200) * size + 200;
-    y = sin(timer * 200) * size + 200;
-    ellipseSize = 20;
-    fill("white");
-    stroke("black");
-    // textSize(20);
-    // text("fps:" + fps, 10, 50);
-    // strokeWeight(1);
-    noStroke();
-    fill("#e0e0e0");
-    ellipse(200, 200, 20);
-    ellipse(x, y, ellipseSize);
-    // line(200, 200, 200, y);
-    // line(200, 200, x, 200);
-    // line(200, y, x, y);
-    // line(x, 200, x, y);
-    colorMode(HSB);
-    stroke((2 * frameCount) % 360, 40, 100);
-    colorMode(RGB);
-    strokeWeight(5);
-    fill("red");
-    line(200, y, x, 200);
-    fill("black");
-    // noLoop();
+  // lines("#000");
+  frameRate(fps);
+  fps = fpsSlider.value;
+  timer += speed;
+  x = cos(timer * 200) * size + 200;
+  y = sin(timer * 200) * size + 200;
+  ellipseSize = 20;
+  fill("white");
+  stroke("black");
+  noStroke();
+  fill("#e0e0e0");
+  ellipse(200, 200, 20);
+  ellipse(x, y, ellipseSize);
+  colorMode(HSB);
+  stroke((2 * frameCount) % 360, 40, 100);
+  colorMode(RGB);
+  strokeWeight(5);
+  fill("red");
+  line(200, y, x, 200);
+  fill("black");
+  // noLoop();
 }
 
 function reset() {
-    background("#fff");
-    // lines("white");
+  background("#fff");
+  // lines("white");
 }

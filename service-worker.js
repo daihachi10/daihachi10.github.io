575a78237d9499201df84f33eb98d739d2796228
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-10-19 21:24:59 +0900

    マニフェスト、サービスワーカー、サイトマップ更新
    マニフェスト、サービスワーカーはhttps://www.pwabuilder.com/reportcard?site=https://daihachi10.github.io
    使った

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-﻿// service-worker.js
+﻿/*
 const CACHE_NAME = 'offline-cache-v1';
 const OFFLINE_URL = 'https://daihachi10.github.io/offline/'
-/* const OFFLINE_CSS = 'https://daihachi10.github.io/offline/offline.css'; */
 const ASSETS = [
   '/',
   OFFLINE_URL,
@@ -10,7 +9,6 @@ const ASSETS = [
     'https://daihachi10.github.io/beta/offline/offline.css',
 ];
 
-// インストールイベント
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -20,7 +18,6 @@ self.addEventListener('install', event => {
     );
 });
 
-// フェッチイベント
 self.addEventListener('fetch', event => {
     if (event.request.mode === 'navigate') {
         event.respondWith(
@@ -45,3 +42,59 @@ self.addEventListener('fetch', function(event) {
     );
 });
 
+*/
+
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add(offlineFallbackPage))
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+
+                if (preloadResp) {
+                    return preloadResp;
+                }
+
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match(offlineFallbackPage);
+                return cachedResp;
+            }
+        })());
+    }
+});

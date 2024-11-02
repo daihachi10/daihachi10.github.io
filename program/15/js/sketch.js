ef779dcee0df2c5337ee34f9a536a461b276cf8c
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-11-02 18:29:57 +0900

    ここまでグアムだけなんかあったらここに、、、

--- program/15/js/sketch.js
+++ program/15/js/sketch.js
@@ -14,34 +14,42 @@ let odai = [
 
 let guamu = [
     "g", "u", "a", "m", "u", "t", "o", "s", "a", "i", "p", "a", "n", "h", "a", "t", "i", "k", "a", "i",
+    // "t", "a", "k", "k", "u", "u", "d", "e", "d", "a", "k", "k", "y", "u", "u",
 ]
-
+let isKeyPushed = false;
+// let keyIsReleased = true;  // フラグを追加
 let imanoyatu = 0;
 let keys;
 let i = 0
 let sushiImage;
+let sushi_karaImage;
 let countUpTimer = 0;
 let countDownTimer;
 let time = 60
 let sushiX = -103
 let sushiSpeed = 1;
-
+let score = 0;
+let sara = 0;
+let juunokuraiSara = 0;
 function preload() {
     sushiImage = loadImage('image/sushi.webp');
-    // sushiImage = resize(10,10)
-
+    sushi_karaImage = loadImage('image/sushi_kara.webp');
 }
 
 function setup() {
+    // frameRate(180)
     let canvasContainer = document.getElementById("p5-canvas-container");
     let canvas = createCanvas(400, 300);
     canvas.parent(canvasContainer); // コンテナにキャンバスを配置
     typing();
     noStroke();
+    angleMode(DEGREES);//半円作るのに使う
+    frameRate(60)
 }
 
 function draw() {
-    background(105);
+    background(255);
+
     timers();
     backgrounds();
     sushi();
@@ -51,6 +59,8 @@ function draw() {
         oldHyouji(n);
     }
     keys = guamu[i]
+
+    //長押しでも反応してしまう
     if (key === keys) {
         console.log("nyuryoku:" + key)
         i += 1;
@@ -59,14 +69,30 @@ function draw() {
     } else {
         iscorrect = false
     }
-    if (keyIsPressed === true) {
-    }
+    //長押しでも反応しないけど早く入力すると反応しない
+    // if (keyIsPressed && !isKeyPushed && key === keys) {
+    //     console.log("nyuryoku:" + key);
+    //     i += 1;
+    //     keys = guamu[i];
+    //     iscorrect = true;
+    //     isKeyPushed = true;  // キーが押されたときにフラグをリセット
+    // } else if (!keyIsPressed) {
+    //     isKeyPushed = false;  // キーが離されたらフラグを元に戻す
+    // }
+    // console.log(keyIsPressed);
+
+
+
     if (i == guamu.length) {
         // すべて打ったあとの処理
         sushiX = -103
         sushiSpeed += 0.07
         next();
+        score++;
+        sara++;
     }
+    sushiSet()
+
 }
 
 function typing() {
@@ -100,7 +126,31 @@ function gui() {
     fill("#000");
     textSize(20);
     text("残り" + countDownTimer + "秒", 60, 20);
+    // 左右の線
+    fill("#f79a4f");
+    rect(0, 40, 5, 230);
+    rect(400, 40, -5, 230);
+    // スコア
+    fill("#f29e4f")
+    rect(0, 270, 400, 50, 0, 0, 5, 5)
 
+    if (score >= 10) {
+        //お皿の数のもの
+        fill("#fff");
+        rect(8, 185, 46, 25, 5);
+        fill("#cbcccb");
+        arc(30, 214, 20, 20, 180, 360);
+        stroke("#cbcccb");
+        strokeWeight(2);
+        line(30, 214, 30, 235);
+        noStroke();
+        ellipse(30, 235, 30, 5);
+        //お皿の数
+        fill("#000")
+        textSize(15);
+        text(Math.round(score / 10) * 10 + "皿", 30, 197)
+    }
+    textSize(18)
 }
 
 function sushi() {
@@ -140,5 +190,18 @@ function backgrounds() {
     rect(0, 144, 400, 70);
     // 机
     fill("#eab768");
-    rect(0, 214, 400);
+    rect(0, 214, 400, 370);
+
+}
+
+function sushiSet() {
+    if (sara >= 10) {
+        sara = 0
+    }
+    // sara++;
+
+    for (n = 0; n < sara; n += 1) {
+        sushi_karaImage.resize(0, 50)
+        image(sushi_karaImage, 200 - 50.5, 210 - n * 5)
+    }
 }

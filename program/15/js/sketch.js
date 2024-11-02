8fc7b800e26df69e93b13152c56d5ef5a1d01974
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-11-02 14:51:09 +0900

    Update sketch.js

--- program/15/js/sketch.js
+++ program/15/js/sketch.js
@@ -12,20 +12,6 @@ let odai = [
     // "ここで会ったが百年目",
 ];
 
-let nihongo = [
-    "guamutosaipanhatikai",
-    // "takkyuudedakkyuu",
-    // "yuukyuukyuukawotukau",
-    // "shehunokimaguresarada",
-    // "kyaramerupoppuko-n",
-    // "usagiwomohumohusuru",
-    // "hudangihapajamadesu",
-    // "aitakutigahusagaranai",
-    // "madonosotohatumetaiame",
-    // "amerikansho-tohea-",
-    // "kokodeattagahyakunenme",
-];
-
 let guamu = [
     "g", "u", "a", "m", "u", "t", "o", "s", "a", "i", "p", "a", "n", "h", "a", "t", "i", "k", "a", "i",
 ]
@@ -34,6 +20,11 @@ let imanoyatu = 0;
 let keys;
 let i = 0
 let sushiImage;
+let countUpTimer = 0;
+let countDownTimer;
+let time = 60
+let sushiX = -103
+let sushiSpeed = 1;
 
 function preload() {
     sushiImage = loadImage('image/sushi.webp');
@@ -46,12 +37,13 @@ function setup() {
     let canvas = createCanvas(400, 300);
     canvas.parent(canvasContainer); // コンテナにキャンバスを配置
     typing();
-    textAlign(CENTER, CENTER);
     noStroke();
 }
 
 function draw() {
-    background(100);
+    background(105);
+    timers();
+    backgrounds();
     sushi();
     gui();
     odaihyouji();
@@ -71,6 +63,9 @@ function draw() {
     }
     if (i == guamu.length) {
         // すべて打ったあとの処理
+        sushiX = -103
+        sushiSpeed += 0.07
+        next();
     }
 }
 
@@ -80,9 +75,9 @@ function typing() {
 }
 
 function odaihyouji() {
-    text(odai[imanoyatu], 200, 135);
-    // text(nihongo[imanoyatu], 10, 20);
     fill("#fff");
+    text(odai[imanoyatu], 200, 135);
+    textSize(13);
     for (let n = 0; n < guamu.length; n += 1) {
         text(guamu[n], 10 + n * 9 + (10 * guamu.length / 2), 160);
     }
@@ -91,17 +86,49 @@ function odaihyouji() {
 function oldHyouji(n) {
     fill("#828282");
     text(guamu[n], 10 + n * 9 + (10 * guamu.length / 2), 160);
-    // if (iscorrect === true) {
-    // }
 }
 
 function gui() {
+
+    //中央の文字の下の黒い四角
+    textAlign(CENTER, CENTER);
     fill(0, 0, 0, 200);
     rect(50, 110, 300, 70, 7);
-    fill("#fff");
+
+    //残り時間
+    fill("#000");
+    textSize(20);
+    text("残り" + countDownTimer + "秒", 60, 20);
+
 }
 
 function sushi() {
-    image(sushiImage, 0, 70)
-    sushiImage.resize(0, 50)
+    if (sushiX < 400) {
+        sushiImage.resize(0, 50)
+        image(sushiImage, sushiX, 70)
+    } else {
+        sushiX = -103
+        next();
+    }
+    sushiX += sushiSpeed;
+}
+function timers() {
+    countUpTimer += 1 / 60;
+    countDownTimer = time - countUpTimer.toFixed(0);
+}
+
+function next() {
+    i = 0;
+}
+
+function backgrounds() {
+    //残り時間の下のやつ
+    fill("#ffa65a");
+    rect(0, 0, 400, 40, 5, 5, 0);
+    //寿司のレーン
+    fill("#efbf6b");
+    rect(0, 60, 400, 70)
+    //背景の緑色のやつ
+    fill("#b5bf65")
+    rect(0, 40, 400, 45);
 }

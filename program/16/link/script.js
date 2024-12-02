b022e544fcc2664a64b92025e4efbafdae2d7c70
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-12-02 21:40:08 +0900

    Update script.js

--- program/16/link/script.js
+++ program/16/link/script.js
@@ -21,8 +21,8 @@ const dontShowAgainCheckbox = document.getElementById("dont-show-again");
 
 // ローカルストレージを確認
 if (localStorage.getItem("skipConfirmation") === "true" && targetUrl) {
-    // window.location.href = targetUrl; 
     window.open(targetUrl, "_blank"); // 新しいタブで開く
+    window.location.href = "../index.html";
 } else {
     // パラメーターがない場合の処理
     if (!targetUrl) {

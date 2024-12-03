98390405bde0e8ca5783e923be5e09541345b870
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-12-03 21:26:39 +0900

    update

--- program/16/iframe/script.js
+++ program/16/iframe/script.js
@@ -4,3 +4,16 @@ const loading = document.getElementById('loading');
 window.onload = function () {
     loading.classList.add('loaded');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-slider', {
+        type: 'loop',
+        perPage: 1,
+        padding: '6rem',
+        drag: 'free',
+        snap: true,
+        lazyLoad: 'sequential',
+        Keyboard: true,
+        // wheel: true,
+    }).mount();
+});

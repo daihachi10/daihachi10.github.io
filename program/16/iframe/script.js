5c28724d86b2fbc42cf8995b0457d0848e6b8847
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-12-05 21:18:06 +0900

    update

--- program/16/iframe/script.js
+++ program/16/iframe/script.js
@@ -17,28 +17,3 @@ document.addEventListener('DOMContentLoaded', function () {
         // wheel: true,
     }).mount();
 });
-
-//cursor
-const cursor = document.querySelector('.cursor');
-const cursorFollower = document.querySelector('.cursor-follower');
-
-let mouseX = 0, mouseY = 0;
-let followerX = 0, followerY = 0;
-
-document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-    cursor.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
-});
-
-function animateFollower() {
-    const speed = 0.2; // 遅延速度
-    followerX += (mouseX - followerX) * speed;
-    followerY += (mouseY - followerY) * speed;
-
-    cursorFollower.style.transform = `translate(${followerX}px, ${followerY}px)`;
-    requestAnimationFrame(animateFollower);
-}
-
-animateFollower();

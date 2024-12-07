e388db571bd38fc7374172ef790a85af89ce336c
daihachi <134046242+daihachi10@users.noreply.github.com>
2024-12-07 20:33:39 +0900

    update

--- program/16/iframe/script.js
+++ program/16/iframe/script.js
@@ -1,3 +1,15 @@
+// 現在のURLからパラメーターを取得
+const theme = new URLSearchParams(window.location.search);
+
+// 特定の値に応じてクラスを追加する例
+if (theme.get('theme') === 'dark') {
+    document.body.classList.add('dark');
+} else if (theme.get('theme') === 'light') {
+    document.body.classList.remove('dark');
+} else {
+    document.body.classList.remove('dark');
+}
+
 //loading処理
 const loading = document.getElementById('loading');
 

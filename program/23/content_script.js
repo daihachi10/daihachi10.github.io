5d81b041489ae1052df66ab232f029a460367065
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-01-04 21:20:48 +0900

    up

---
+++ program/23/content_script.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', function (event) {
+    // 押されたキーに基づいて処理を分岐
+    if (event.key === '\\' && event.ctrlKey) {
+        window.location.href = 'https://daihachi10.github.io/program/22/';
+    }
+
+})

a15f1f64e7d787bdac7bdecdd7d9ae10a1cda338
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-02-14 20:37:37 +0900

    up

--- common/script.js
+++ common/script.js
@@ -1,5 +1,5 @@
 $(function () {
     $(".header").load("https://daihachi10.github.io/common/header.html");
     $(".footer").load("https://daihachi10.github.io/common/footer.html");
-    // $("#loading").load("https://daihachi10.github.io/common/loading.html");
+    $("#loading").load("https://daihachi10.github.io/common/loading.html");
 });

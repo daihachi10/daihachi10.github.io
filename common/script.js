6a423ab61da162c7ec0024f417fad32b93247757
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-02-14 09:49:57 +0900

    埋め込み

---
+++ common/script.js
@@ -0,0 +1,5 @@
+$(function () {
+    $(".header-iframe-box").load("https://daihachi10.github.io/common/header.html");
+    $(".footer-iframe-box").load("https://daihachi10.github.io/common/footer.html");
+    $(".loading-iframe-box").load("https://daihachi10.github.io/common/loading.html");
+});

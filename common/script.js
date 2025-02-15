689b1b0f5d154bf1cdf62fe2226d491c57f4d685
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-02-15 18:16:12 +0900

    up
    Co-Authored-By: daihachi10sub <174610321+daihachi10sub@users.noreply.github.com>

--- common/script.js
+++ common/script.js
@@ -3,3 +3,10 @@ $(function () {
     $("#loading").load("https://daihachi10.github.io/common/loading.html");
     $(".header").load("https://daihachi10.github.io/common/header.html");
 });
+
+document.addEventListener("click", function(event) {
+    // クリックされたときの処理
+    console.log("クリックされました！", event.clientX, event.clientY);
+
+    AOS.init();
+});

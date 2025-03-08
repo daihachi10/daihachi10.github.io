20753ceb7938f6ef2d6cd11b6a888ca297ba1185
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-08 18:24:04 +0900

    Update script.js

--- common/script.js
+++ common/script.js
@@ -5,7 +5,8 @@ $(function () {
     $(".header").load("https://daihachi10.github.io/common/header.html");
 
     $.get("https://daihachi10.github.io/common/color.html", function(data){
-        $("body").append(data);
+        $("body").prepend(data); // 先頭に追加する場合
+        // $("body").append(data); // 末尾に追加する場合は、こちらを使用
       });
 });
 

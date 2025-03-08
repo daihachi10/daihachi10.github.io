e3728449b61b952a5c3a46e1f671f6734a37527d
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-08 09:58:18 +0900

    up

--- program/33/js/game.js
+++ program/33/js/game.js
@@ -2,12 +2,18 @@ let grid = 15;
 let gridSize = 512 / grid;
 
 let orientation = 0;
-let speed = 5; //34.1
+let speed = 4; //34.1
 let playerX = 512 / 2 - 17; // width / 2 - 間隔 / 2
 let playerY = 512 / 2 - 17;
+let playerColor = "#4674e1"
+
+let oldPlayerX = [playerX];
+let oldPlayerY = [playerY];
 
 let appleX = 0;
 let appleY = 0;
+
+let score = 0;
 
 function setup() {
     // createCanvas(512, 512);
@@ -15,6 +21,9 @@ function setup() {
     let canvas = createCanvas(512, 512);
     canvas.parent(canvasContainer); // コンテナにキャンバスを配置
     frameRate(60);
+    noStroke();
+    appleX = random(512);
+    appleY = random(512);
 }
 
 function draw() {
@@ -22,10 +31,12 @@ function draw() {
     background("#abd55b");
     drawLine();
     judgment();
+    crone(score);
     playerSpan(playerX, playerY);
     appleSpan(appleX, appleY);
     move(orientation);
     game();
+    drawScore();
 }
 
 function judgment() {
@@ -96,20 +107,36 @@ function drawLine() {
     for (let y = 0; y <= height; y += width / grid) {
         line(0, y, width, y);
     }
+    noStroke();
 }
 
 function playerSpan(x, y) {
     let size = 7;
+    fill(playerColor);
     rect(x + size, y + size, gridSize - size * 2, gridSize - size * 2);
+}
+
+function crone() {
+
+    for (let i = 0; i < score + 20; i++) {
+        let size = 7;
+        let x = oldPlayerX[oldPlayerX.length - i];
+        let y = oldPlayerY[oldPlayerX.length - i];
+        fill(playerColor);
+
+        rect(x + size, y + size, gridSize - size * 2, gridSize - size * 2);
+        fill("#fff")
+    }
 }
 
 function appleSpan(x, y) {
     let size = 4;
 
-    let revision = 0.1;
+    let revision = 1;
     let standardRevision = 1;
+    fill("#f00")
     rect(x + size, y + size, gridSize - size * 2, gridSize - size * 2);
-
+    fill("#fff")
     let detection = 15
     if (
         playerX - appleX < detection &&
@@ -117,6 +144,10 @@ function appleSpan(x, y) {
         playerY - appleY < detection &&
         playerY - appleY > -detection
     ) {
+        score++
+        if (score % 2 === 0) {
+            speed += 0.5;
+        }
         appleX = random(512);
         appleY = random(512);
     }
@@ -151,10 +182,13 @@ function move(i) {
             playerY += speed;
             break;
     }
+
+    oldPlayerX.push(playerX);
+    oldPlayerY.push(playerY);
 }
 
 function game() {
-    console.log(playerY)
+    // console.log(playerY)
     if (playerX >= 512 || playerX <= 0 - gridSize || playerY >= 512 || playerY <= 0 - gridSize) {
         fill("#ff0000");
         textSize(50);
@@ -162,4 +196,12 @@ function game() {
         text("GameOver", width / 2, height / 2);
         noLoop();
     }
+}
+
+function drawScore() {
+    fill("#420e0e");
+    textSize(20);
+    textAlign(LEFT, TOP);
+    text("score: " + score, 10, 10);
+    fill("#fff");
 }

f9c9f28521d9ce2af62fc576a41c9cc2c7555753
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-10 09:44:51 +0900

    update

--- program/common/script.js
+++ program/common/script.js
@@ -2,3 +2,7 @@ $(function () {
     $(".shop-menu").load("../common/sidebar.html");
     $(".item-list").load("../common/recommended/index.html");
 });
+
+function hidden() {
+    $("#sidebar").addClass("hidden");
+}

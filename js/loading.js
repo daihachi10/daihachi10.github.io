054951162176134cc9c7d9d804af42858801b2e5
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-19 07:59:16 +0900

    up

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function () {
     // ページが完全に読み込まれたらローディング画面を非表示にしてコンテンツを表示
+    setTimeout(function(){
     document.getElementById('loading').style.display = 'none';
     document.getElementById('content').style.display = 'block';
+
+    },1000);
+
 });

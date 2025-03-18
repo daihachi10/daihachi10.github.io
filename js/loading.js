d147483fc2d8b66fa1e9159aa1c475f7c46a0ca9
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-19 08:00:33 +0900

    i

--- js/loading.js
+++ js/loading.js
@@ -4,6 +4,6 @@ window.addEventListener('load', function () {
     document.getElementById('loading').style.display = 'none';
     document.getElementById('content').style.display = 'block';
 
-    },1000);
+    },4000);
 
 });

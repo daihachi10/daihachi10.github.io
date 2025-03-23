eb27dddabf2478ed24b4d2ae3c06b41cd2475e12
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-23 17:59:32 +0900

    up

--- common/script.js
+++ common/script.js
@@ -1,8 +1,8 @@
 $(function () {
   $(".footer").load("https://daihachi10.github.io/common/footer.html");
   $("#loading").load("https://daihachi10.github.io/common/loading.html");
-  //   $(".header").load("https://daihachi10.github.io/common/header.html");
-  $(".header").load("./common/header.html");
+  $(".header").load("https://daihachi10.github.io/common/header.html");
+  //   $(".header").load("./common/header.html");
 
   // $("#login").load("./account/iframe.html");
 

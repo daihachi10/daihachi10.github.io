78506890ffcc96e987b161a5e3ef8ce7fc752393
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-03-26 16:51:17 +0900

    up

---
+++ program/35/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello World!!');
+const MESSAGE = "Hello World!";
+function add(x, y) {
+    return x + y;
+}
+console.log(add(1, 1));
---
+++ program/35/src/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello World!!');
+const MESSAGE: string = "Hello World!"
+function add(x: number, y: number): number {
+    return x + y
+
+}
+
+console.log(add(1,1))

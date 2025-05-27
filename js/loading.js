e232a33126cd08381bbf77d8f992bb82248d9928
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-05-27 21:40:08 +0900

    loading

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,5 @@
-window.addEventListener('load', function () {
-    // ページが完全に読み込まれたらローディング画面を非表示にしてコンテンツを表示
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+window.addEventListener("load", function () {
+  // ページが完全に読み込まれたらローディング画面を非表示にしてコンテンツを表示
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("content").style.display = "block";
 });

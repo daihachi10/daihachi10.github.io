c45ad46597134cb1524935de74aa8e4c0d009c35
daihachi <134046242+daihachi10@users.noreply.github.com>
2025-05-28 09:17:21 +0900

    loading

--- common/program.js
+++ common/program.js
@@ -1,6 +1,6 @@
 $(function () {
   $(".footer").load("https://daihachi10.github.io/common/footer.html");
-  $("#loading").load("https://daihachi10.github.io/common/loading.html");
+  // $("#loading").load("https://daihachi10.github.io/common/loading.html");
   $(".header").load("https://daihachi10.github.io/common/programheader.html");
   //   $(".header").load("../../common/programheader.html");
 
